cc31843c264bb061ff8d28ad559021a04f6008e0
Shivani Shukla <shukla.shivani2009@gmail.com>
2018-08-28 02:18:03 -0700

    adding changes to Two Sum

--- TwoSum_lc.py
+++ TwoSum_lc.py
@@ -1,3 +1,4 @@
+# Runtime in O(n^2),Space: O(1)
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -10,3 +11,26 @@ class Solution(object):
             for j in range(i+1,length):
                 if nums[i]+nums[j]==target:
                     return [i,j]
+
+
+"""
+LeetCode solution in O(n),Space:O(n)
+"""
+class Solution_1(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        Given two sum, we can iterate and try out every combination. This will cause n^2 times.
+        else we can use hashmap to store each number as key and index as value and try to see if the target sum exists.
+        """
+        dic = {}
+        for i, val in enumerate(nums):
+            if target - val in dic:
+                return [i, dic[target - val]]
+            dic[val] = i
+
+
+print(Solution().twoSum([2,4,5,8],13))
+print(Solution_1().twoSum([2,4,5,8],13))
