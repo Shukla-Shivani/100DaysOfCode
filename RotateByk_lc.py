faee3387199288c13b87fa9f0a3f14fabbcf9875
Shivani Shukla <shukla.shivani2009@gmail.com>
2018-09-16 13:19:13 -0700

    rotate to right by k steps

---
+++ RotateByk_lc.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        arr = []
+        i = 0
+        length = len(nums)
+
+        k = k%length
+
+        kstart = length-k
+        for j in range (kstart, length):
+            arr.append(nums[j])
+
+        for j in range(0, kstart):
+            arr.append(nums[j])
+
+        for x in arr:
+            nums[i] = x
+            i = i+1
