f4733415e490cf59e6d8bd3c5c26cf03a30fd243
Shivani Shukla <shukla.shivani2009@gmail.com>
2019-02-05 17:54:00 -0800

    maximum sum of contiguous subarray

---
+++ MaximumSubarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        
+        max_sum = nums[0]
+        current_sum = nums[0]
+        
+        for i in range(1, len(nums)):
+            current_sum = max(current_sum + nums[i], nums[i])
+            max_sum = max(max_sum, current_sum)
+        return max_sum
+        
+        
